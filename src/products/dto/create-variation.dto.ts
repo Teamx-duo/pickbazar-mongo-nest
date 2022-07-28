25120b04e6786df76e338d0236cf370de6947043
Usama Irfan <usama.irfan9797@gmail.com>
2022-07-29 02:39:47 +0500

    Changes to variations and shops controllers

---
+++ src/products/dto/create-variation.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { IsArray, IsNotIn, IsOptional, IsString } from 'class-validator';
+import { Category } from '../schemas/category.schema';
+
+export class CreateVariationDto {
+  @IsString()
+  public name: string;
+  @IsString()
+  public type: string;
+  @IsString()
+  public details: string;
+  @IsString()
+  @IsOptional()
+  public parent: string;
+  @IsString()
+  @IsOptional()
+  public image: string;
+  @IsString()
+  @IsOptional()
+  public icon: string;
+}
+
+export class CreateCategoryRequestDto extends PickType(Category, [
+  'name',
+  'details',
+  'parent',
+]) {}
