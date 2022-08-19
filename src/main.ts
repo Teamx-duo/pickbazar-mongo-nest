45ddc04eb60c6cd16983fc7fd68765222d0b01c7
Usama Irfan <usama.irfan9797@gmail.com>
2022-08-19 16:32:58 +0500

    Test changes

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      // forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalFilters(new AllExceptionsFilter());
   const config = new DocumentBuilder()
     .setTitle('Nest Multivendor')
